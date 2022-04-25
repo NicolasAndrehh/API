f61c2111a0cf2e7286387c91174eac762d0c7fa5
Nicolas Olaya <naolay@misena.edu.co>
2022-04-25 11:49:29 -0500

    a

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -1,5 +1,13 @@
-let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+// let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 let API = 'https://rickandmortyapi.com/api/character/';
+
+// let tabla = document.getElementById('tabla');
+// let idInput = document.getElementById('id');
+// let nombreInput = document.getElementById('nombre');
+// let estadoInput = document.getElementById('estado');
+// let especieInput = document.getElementById('especie');
+// let generoInput = document.getElementById('genero');
+// let dimensionInput = document.getElementById('dimension');
 
 function fetchData(url_api, callback){
     let xhttp = new XMLHttpRequest();
@@ -21,22 +29,42 @@ function fetchData(url_api, callback){
     xhttp.send();
 }
 
-fetchData(API, function(error1, data1){
-    if(error1) return console.log(error1);
+for (let i = 0; i < 5; i++) {
 
-    fetchData(API + data1.results[0].id, function(error2, data2){
-        if(error2) return console.log(error2);
-        
-        fetchData(data2.origin.url, function(error3, data3){
-            if(error3) return console.log(error3);
+    
+    fetchData(API, function(error1, data1){
+        if(error1) return console.log(error1);
+    
+        fetchData(API + data1.results[i].id, function(error2, data2){
+            if(error2) return console.log(error2);
             
-            console.log(data1.info.count);
-            console.log(data2.name);
-            console.log(data3.dimension);
+            fetchData(data2.origin.url, function(error3, data3){
+                if(error3) return console.log(error3);
+                
+                let infoCount = (data1.info.count);
+                let id = (data2.id);
+                let nombre = (data2.name);
+                let estado = (data2.status);
+                let especie = (data2.species);
+                let genero = (data2.gender);
+                let dimension = (data3.dimension);
 
-            console.log(API);
-            console.log(API + data1.results[0].id);
-            console.log(data2.origin.url);
+
+                tabla.innerHTML += `<tr><td id='id'>${id}</td><td id='nombre'>${nombre}</td><td id='estado'>${estado}</td><td id='especie'>${especie}</td><td id='genero'>${genero}</td><td id='dimension'>${dimension}</td></tr>`
+    
+                // idInput.innerText = id;
+                // nombreInput.innerText = nombre;
+                // estadoInput.innerText = estado;
+                // especieInput.innerText = especie;
+                // generoInput.innerText = genero;
+                // dimensionInput.innerText = dimension;
+    
+                console.log(API);
+                console.log(API + data1.results[0].id);
+                console.log(data2.origin.url);
+            })
         })
     })
-})
+
+    
+}    
